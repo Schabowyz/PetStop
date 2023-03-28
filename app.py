b3ebe140a6cfd962571b3e008ee1f030610f71d3
Schabowyz <ddamian.19962@gmail.com>
2023-03-28 20:15:16 +0200

    modified:   __pycache__/app.cpython-310.pyc modified:   __pycache__/helpers.cpython-310.pyc modified:   app.py modified:   database.db modified:   helpers.py modified:   templates/layout.html modified:   templates/register.html new file:   templates/registershelter.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 import sqlite3
 
-from helpers import check
+from helpers import check_email, conv_tup_to_str
 
 # Configure application
 app = Flask(__name__)
@@ -37,12 +37,64 @@ def add():
 
         return redirect("/")
     
-    
-    
+
 @app.route("/register", methods = ["GET", "POST"])
 def register():
+    error = None
+    if request.method == "POST":
+        # Opens database and creates a cursor
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+
+        # Gets informations from form and checks if them for faults
+
+        # Checks username
+        username = request.form.get("username")
+        print(username)
+        
+        if len(username) < 4 or len(username) > 16:
+            error = "Username must be between 4 and 16 characters long!"
+
+        if not username.isalnum():
+            error = "Username must contain only letters and numbers!"
+
+        cur.execute("SELECT username FROM users WHERE username = ?", (username, ))
+        if username == conv_tup_to_str(cur.fetchone()):
+            error = "Username already taken!"
+
+        
+        # Checks email
+        email = request.form.get("email")
+        print(email)
+
+        if check_email(email) == False:
+            error = "Incorrect email adress!"
+
+        cur.execute("SELECT email FROM users WHERE email = ?", (email, ))
+        if email == conv_tup_to_str(email):
+            error = "Email adress already taken!"
+
+
+        # Checks password
+        password = request.form.get("password")
+        passwordcon = request.form.get("passwordcon")
+
+        if password != passwordcon:
+            error = "Passwords don't match!"
+
+        if len(password) < 8 or len(password) > 20:
+            error = "Password must be between 8 and 20 characters long"
+
+    
+    return render_template("register.html", error=error)
+
+
+
+    
+@app.route("/registershelter", methods = ["GET", "POST"])
+def registershelter():
     if request.method == "GET":
-        return render_template("register.html")
+        return render_template("registershelter.html")
 
     else:
         # Opens database and creates a cursor
@@ -53,6 +105,7 @@ def register():
 
         # Check name
         name = request.form.get("name")
+
         if name.isalnum() == False:
             print("Unallowed characters in name")
             return redirect("/")
@@ -63,24 +116,70 @@ def register():
         
         cur.execute("SELECT name FROM shelters")
         names = cur.fetchall()
-        x = isinstance(names[1], tuple)
-        print(names)
-        print(x)
-        # if name in names.values():
-        #     print("Username not available")
-        #     return redirect("/")
+        names = [item for t in names for item in t]
+        if name in names:
+            print("Username not available")
+            return redirect("/")
         
 
         # Check email
         email = request.form.get("email")
-        if check(email) == False:
+
+        if check_email(email) == False:
             print("Invalid email")
             return redirect("/")
         
-        # emails = cur.execute("SELECT con_email FROM shelters")
-        # if email in emails.values():
-        #     print("Email already taken")
-        #     return redirect("/")
+        cur.execute("SELECT con_email FROM shelters")
+        emails = cur.fetchall()
+        emails = [item for t in emails for item in t]
+        if email in emails:
+            print("Email already taken")
+            return redirect("/")
+        
+
+        # Check password
+        password = request.form.get("password")
+        conpas = request.form.get("conpas")
+
+        if password != conpas:
+            print("Passwords don't match")
+            return redirect("/")
+        
+        if len(password) < 8 or len(password) > 30:
+            print("Password must contain between 8 and 30 characters")
+            return redirect("/")
+        
+
+        # Check city
+        city = request.form.get("city")
+
+        if city.isalpha() == False or not city:
+            print("Please provide right city name")
+            return redirect("/")
+        
+
+        # Check adress
+        adress = request.form.get("adress")
+
+        if not adress:
+            print("Please provide the right adress")
+            return redirect("/")
+        
+
+        # Check postal code
+        postal = request.form.get("postal")
+
+        if not postal:
+            print("Please provide the right postal code")
+            return redirect("/")
+        
+
+        # Check number
+        phone = request.form.get("phone")
+
+        if not phone:
+            print("Please provide the right phone number")
+            return redirect("/")
 
 
     print("Alls good")
--- helpers.py
+++ helpers.py
@@ -1,10 +1,14 @@
 from email_validator import validate_email, EmailNotValidError
 
 
-def check(email):
+def check_email(email):
     try:
         validate = validate_email(email)
         email = validate["email"]
     except EmailNotValidError:
         return False
     return True
+
+def conv_tup_to_str(tup):
+    string = ''.join(tup)
+    return string
